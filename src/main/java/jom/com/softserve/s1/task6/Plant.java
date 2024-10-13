5541e946c6ea2f611f2d74fd515fdf80ef92d7a2
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-10-13 11:37:24 +0200

    Ok

--- src/main/java/jom/com/softserve/s1/task6/Plant.java
+++ src/main/java/jom/com/softserve/s1/task6/Plant.java
@@ -1,5 +1,120 @@
 package jom.com.softserve.s1.task6;
 
-public class Plant {
-
+enum Color {
+    WHITE, RED, BLUE
 }
+
+enum Type {
+    RARE, ORDINARY
+}
+
+ class ColorException extends Exception {
+    public ColorException(String color) {
+        super("Invalid value " + color + " for field color");
+    }
+}
+
+ class TypeException extends Exception {
+    public TypeException(String type) {
+        super("Invalid value " + type + " for field type");
+    }
+}
+
+public class Plant {
+    private Type type;
+    private Color color;
+    private String name;
+
+
+    public Plant(String type, String color, String name) throws TypeException, ColorException {
+        try {
+            this.type = Type.valueOf(type.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new TypeException(type);
+        }
+
+        try {
+            this.color = Color.valueOf(color.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new ColorException(color);
+        }
+
+        this.name = name;
+    }
+
+    public static Plant tryCreatePlant(String type, String color, String name) {
+        Plant plant;
+        try {
+            plant = new Plant(type, color, name);
+        } catch (TypeException e) {
+            try {
+                plant = new Plant("ORDINARY", color, name);
+            } catch (ColorException exception) {
+                try {
+                    plant = new Plant(type, "RED", name);
+
+                } catch (ColorException | TypeException ex) {
+                    try {
+                        plant = new Plant("ORDINARY", "Red", name);
+
+                    } catch (Exception exc) {
+                        throw new IllegalArgumentException();
+                    }
+                }
+            } catch (TypeException ex) {
+                throw new IllegalArgumentException(ex);
+            }
+        } catch (ColorException e) {
+            try {
+                plant = new Plant(type, "RED", name);
+            } catch (TypeException te) {
+                try {
+                    plant = new Plant("ORDINARY", "Red", name);
+                } catch (Exception exc) {
+                    throw new IllegalArgumentException(exc);
+                }
+            } catch (ColorException ex) {
+                throw new IllegalArgumentException(ex);
+            }
+        } catch (Exception ex) {
+            throw new IllegalArgumentException(ex);
+        }
+
+        return plant;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+               "type: " + type + ", " +
+               "color: " + color + ", " +
+               "name: " + name +
+               "}";
+    }
+}
+
+
--- src/test/java/jom/com/softserve/s1/task6/Task6Test.java
+++ src/test/java/jom/com/softserve/s1/task6/Task6Test.java
@@ -1,10 +1,5 @@
 package jom.com.softserve.s1.task6;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
@@ -16,78 +11,79 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class Task6Test {
 
 	final private static String PLANT = "jom.com.softserve.s1.task6.Plant";
 
-//	@DisplayName("Check that Plant class has 'tryCreatePlant' method")
-//	@Test
-//	void hasTypeDeclaredMethod() {
-//		try {
-//			Class<?> clazz = Class.forName(PLANT);
-//			Method[] methods = clazz.getDeclaredMethods();
-//			boolean isMethod = false;
-//			for (Method m : methods) {
-//				if ("tryCreatePlant".equals(m.getName())) {
-//					assertTrue(Arrays.equals(new Class[] { String.class, String.class, String.class },
-//							m.getParameterTypes()));
-//					isMethod = true;
-//					break;
-//				}
-//			}
-//			assertTrue(isMethod);
-//		} catch (ClassNotFoundException e) {
-//			fail("There is no Plant class");
-//		}
-//	}
-//
-//	@DisplayName("Check that 'tryCreatePlant' method returns object of Plant type")
-//	@Test
-//	void hasMethodReturnType() {
-//		try {
-//			Class<?> clazz = Class.forName(PLANT);
-//			Method[] methods = clazz.getDeclaredMethods();
-//			boolean isMethod = false;
-//			for (Method method : methods) {
-//				if ("tryCreatePlant".equals(method.getName())) {
-//					assertEquals(method.getReturnType(), Plant.class);
-//					isMethod = true;
-//				}
-//			}
-//			assertTrue(isMethod);
-//		} catch (ClassNotFoundException e) {
-//			fail("There is no Plant class");
-//		}
-//	}
-//
-//	@DisplayName("Check that 'tryCreatePlant' method is static")
-//	@Test
-//	void isMethodStatic() {
-//		try {
-//			Class<?> clazz = Class.forName(PLANT);
-//			Method[] methods = clazz.getDeclaredMethods();
-//			boolean isMethod = false;
-//			for (Method method : methods) {
-//				if ("tryCreatePlant".equals(method.getName())) {
-//					assertTrue(Modifier.isStatic(method.getModifiers()));
-//					isMethod = true;
-//				}
-//			}
-//			assertTrue(isMethod);
-//		} catch (ClassNotFoundException e) {
-//			fail("There is no Plant class");
-//		}
-//	}
-//
-//	@DisplayName("Check that 'tryCreatePlant' method returns valid plant")
-//	@ParameterizedTest
-//	@MethodSource("providePlantData")
-//	void isCorrectPlantCreated(String type, String color, String name) {
-//		assertNotNull(Plant.tryCreatePlant(type, color, name));
-//	}
-//
-//	private static Stream<Arguments> providePlantData() {
-//		return Stream.of(Arguments.of("Rare", "MyColor", "MyName"), Arguments.of("MyType", "Red", "MyName"),
-//				Arguments.of("MyType", "MyColor", "MyName"));
-//	}
+	@DisplayName("Check that Plant class has 'tryCreatePlant' method")
+	@Test
+	void hasTypeDeclaredMethod() {
+		try {
+			Class<?> clazz = Class.forName(PLANT);
+			Method[] methods = clazz.getDeclaredMethods();
+			boolean isMethod = false;
+			for (Method m : methods) {
+				if ("tryCreatePlant".equals(m.getName())) {
+                    assertArrayEquals(new Class[]{String.class, String.class, String.class}, m.getParameterTypes());
+					isMethod = true;
+					break;
+				}
+			}
+			assertTrue(isMethod);
+		} catch (ClassNotFoundException e) {
+			fail("There is no Plant class");
+		}
+	}
+
+	@DisplayName("Check that 'tryCreatePlant' method returns object of Plant type")
+	@Test
+	void hasMethodReturnType() {
+		try {
+			Class<?> clazz = Class.forName(PLANT);
+			Method[] methods = clazz.getDeclaredMethods();
+			boolean isMethod = false;
+			for (Method method : methods) {
+				if ("tryCreatePlant".equals(method.getName())) {
+					assertEquals(method.getReturnType(), Plant.class);
+					isMethod = true;
+				}
+			}
+			assertTrue(isMethod);
+		} catch (ClassNotFoundException e) {
+			fail("There is no Plant class");
+		}
+	}
+
+	@DisplayName("Check that 'tryCreatePlant' method is static")
+	@Test
+	void isMethodStatic() {
+		try {
+			Class<?> clazz = Class.forName(PLANT);
+			Method[] methods = clazz.getDeclaredMethods();
+			boolean isMethod = false;
+			for (Method method : methods) {
+				if ("tryCreatePlant".equals(method.getName())) {
+					assertTrue(Modifier.isStatic(method.getModifiers()));
+					isMethod = true;
+				}
+			}
+			assertTrue(isMethod);
+		} catch (ClassNotFoundException e) {
+			fail("There is no Plant class");
+		}
+	}
+
+	@DisplayName("Check that 'tryCreatePlant' method returns valid plant")
+	@ParameterizedTest
+	@MethodSource("providePlantData")
+	void isCorrectPlantCreated(String type, String color, String name) {
+		assertNotNull(Plant.tryCreatePlant(type, color, name));
+    }
+
+	private static Stream<Arguments> providePlantData() {
+		return Stream.of(Arguments.of("Rare", "MyColor", "MyName"), Arguments.of("MyType", "Red", "MyName"),
+				Arguments.of("MyType", "MyColor", "MyName"));
+	}
 }
