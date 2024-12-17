e8521ca5f6b028ad05d74732e1ddf84ab37229c5
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-12-17 21:30:35 +0100

    Implemented product class

--- src/main/java/jom/com/softserve/s1/task3/Product.java
+++ src/main/java/jom/com/softserve/s1/task3/Product.java
@@ -1,5 +1,38 @@
 package jom.com.softserve.s1.task3;
 
-public class Product {
+class Product {
+    private static int count;
+    private String name;
+    private double price;
 
+    public Product() {
+        count++;
+    }
+
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+        count++;
+    }
+
+    public static int count() {
+        return count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
