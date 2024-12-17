5fc2d47220d95fce66e3d1ed01bc5e5582bdbf41
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-12-17 21:34:29 +0100

    Implemented logic

--- src/main/java/jom/com/softserve/s1/task4/Operation.java
+++ src/main/java/jom/com/softserve/s1/task4/Operation.java
@@ -2,12 +2,17 @@ package jom.com.softserve.s1.task4;
 
 class Operation {
 	public static int squareRectangle(int a, int b) {
-		//your code
-		return 0;
+		if (a <= 0 || b <= 0) throw new IllegalArgumentException("Both arguments should be more than zero");
+		return a * b;
 	}
 
 	public static int trySquareRectangle(int a, int b) {
-		//your code
-		return 0;
+		int square;
+		try {
+			square = squareRectangle(a, b);
+		} catch (IllegalArgumentException e) {
+			return - 1;
+		}
+		return square;
 	}
 }
