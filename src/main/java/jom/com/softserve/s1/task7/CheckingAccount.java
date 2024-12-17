330e3e2e4aa9d2c9a5225326816d53cae656f8a4
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-12-17 21:24:33 +0100

    Uncommited CheckingAccount

--- src/main/java/jom/com/softserve/s1/task7/CheckingAccount.java
+++ src/main/java/jom/com/softserve/s1/task7/CheckingAccount.java
@@ -1,23 +1,40 @@
 package jom.com.softserve.s1.task7;
 
 public class CheckingAccount {
-	private double balance;
-	private int number;
+    private double balance;
+    private int number;
 
-	public CheckingAccount(int number) {
-		this.number = number;
-	}
+    public CheckingAccount(int number) {
+        this.number = number;
+    }
 
-	public void deposit(double amount) {
-		balance += amount;
-	}
+    public static void main(String[] args) {
+        doOperations();
+    }
 
-//	public void withdraw(double amount) throws InsufficientAmountException {
-//		if (amount <= balance) {
-//			balance -= amount;
-//		} else {
-//			double needs = amount - balance;
-//			 new InsufficientAmountException(needs);
-//		}
-//	}
+    public static void doOperations() {
+        CheckingAccount c = new CheckingAccount(101);
+        c.deposit(500.00);
+        try {
+            c.withdraw(100.00);
+            c.withdraw(600.00);
+        } catch (InsufficientAmountException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Please, deposit at least $" + e.getAmount());
+            e.printStackTrace();
+        }
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) throws InsufficientAmountException {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            double needs = amount - balance;
+            throw new InsufficientAmountException(needs);
+        }
+    }
 }
